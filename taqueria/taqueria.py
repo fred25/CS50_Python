5ef5ce926548697a3741c563f1747c6c2827dd73
Frederico <fredericomattos25@gmail.com>
2024-02-23 14:33:20 -0300

    taqueria

---
+++ taqueria/taqueria.py
@@ -0,0 +1,21 @@
+MENU = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+total = 0
+while True:
+    try:
+        x = input("Item: ").title()
+        total += MENU[x]
+        print("Total: $%.2f" % total)
+    except EOFError:
+        break
+    except KeyError:
+        pass
